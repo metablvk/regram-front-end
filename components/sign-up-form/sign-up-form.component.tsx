fe2dd2e8636adbe47fcf37faedfa3b94e58039da
metablvk <metablvk@gmail.com>
2023-01-14 12:39:55 -0700

    Refactored sign up form into own component

---
+++ components/sign-up-form/sign-up-form.component.tsx
@@ -0,0 +1,29 @@
+import styles from "./sign-up-form.module.css";
+const SignUpForm = () => {
+  return (
+    <>
+      <div className={`${styles.sign_up_form} container`}>
+        <h1>Sign Up</h1>
+        <form>
+          <div className={styles.form_group}>
+            <input type="text" name="email" required />
+            <label htmlFor="">email</label>
+            <span className={styles.bar}></span>
+          </div>
+          <div className={styles.form_group}>
+            <input type="password" name="confirm_password" required />
+            <label htmlFor="">Password</label>
+            <span className={styles.bar}></span>
+          </div>
+          <div className={styles.form_group}>
+            <input type="password" name="password" required />
+            <label htmlFor="">Confirm Password</label>
+            <span className={styles.bar}></span>
+          </div>
+          <button className="btn">Sign Up</button>
+        </form>
+      </div>
+    </>
+  );
+};
+export default SignUpForm;
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,32 +1,12 @@
 import Layout from "components/layout/layout.component";
 import { NextPage } from "next";
-import styles from "./../styles/sign-up.module.css";
+import SignUpForm from "components/sign-up-form/sign-up-form.component";
 
 const SignUp: NextPage = () => {
   return (
     <>
       <Layout>
-        <div className={`${styles.sign_up_form} container`}>
-          <h1>Sign Up</h1>
-          <form>
-            <div className={styles.form_group}>
-              <input type="text" name="email" required />
-              <label htmlFor="">email</label>
-              <span className={styles.bar}></span>
-            </div>
-            <div className={styles.form_group}>
-              <input type="password" name="confirm_password" required />
-              <label htmlFor="">Password</label>
-              <span className={styles.bar}></span>
-            </div>
-            <div className={styles.form_group}>
-              <input type="password" name="password" required />
-              <label htmlFor="">Confirm Password</label>
-              <span className={styles.bar}></span>
-            </div>
-            <button className="btn">Sign Up</button>
-          </form>
-        </div>
+        <SignUpForm />
       </Layout>
     </>
   );
