fbad7ba86f6d221fc4b5868e27bf7b5936d815a6
metablvk <metablvk@gmail.com>
2023-01-14 01:20:55 -0700

    Built post component

---
+++ components/post/post.component.tsx
@@ -0,0 +1,52 @@
+import { FC } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./post.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faComment } from "@fortawesome/free-solid-svg-icons";
+
+type Post = {
+  img: string;
+  likes: number;
+  username: string;
+  description: string;
+};
+
+type Props = {
+  post: Post;
+};
+const Post: FC<Props> = ({ post }) => {
+  return (
+    <div className={styles.post}>
+      <Image
+        src={post.img}
+        height={200}
+        width={200}
+        alt="Stock image"
+        className={styles.post_img}
+      />
+      <section className={styles.card}>
+        <div className={styles.card_header}>
+          <div>
+            <p>{post.likes} likes</p>
+          </div>
+          <div className={styles.controls}>
+            <FontAwesomeIcon icon={faComment} />
+            <FontAwesomeIcon icon={faHeart} />
+          </div>
+          <div>
+            <p className={styles.card_tag}>{post.username}</p>
+          </div>
+        </div>
+        <div className={styles.card_body}>
+          <p>{post.description}</p>
+        </div>
+        <div className={styles.card_footer}>
+          <Link href="/">View All Comments</Link>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Post;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
 import Head from "next/head";
 import Layout from "components/layout/layout.component";
+import Post from "components/post/post.component";
 export default function Home() {
+  const posts = [
+    {
+      img: "/images/stock-image-1.jpg",
+      likes: 124,
+      username: "alicia",
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis, explicabo mollitia tempore magnam quis.`,
+    },
+    {
+      img: "/images/stock-image-2.jpg",
+      likes: 524,
+      username: "shannon",
+      description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta veritatis, explicabo mollitia tempore magnam quis.`,
+    },
+  ];
   return (
     <>
       <Head>
@@ -9,7 +24,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout></Layout>
+      <Layout>
+        {posts.map((post, id) => (
+          <Post key={id} post={post} />
+        ))}
+      </Layout>
     </>
   );
 }
