ba5819ab176dd9b1a59a24d302047b4acad72e6e
metablvk <metablvk@gmail.com>
2023-01-14 14:18:47 -0700

    Added firebase function to sign up with comments

--- components/sign-up-form/sign-up-form.component.tsx
+++ components/sign-up-form/sign-up-form.component.tsx
@@ -1,23 +1,98 @@
+import { FC, useState, ChangeEvent, FormEvent } from "react";
 import styles from "./sign-up-form.module.css";
+import { createAuthUserWithEmailAndPassword } from "utils/firebase/firebase.utils";
+/*
+ * Form for signing up a user utilizing firebase
+ */
+type FormFields = {
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const defaultFormFields = {
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
 const SignUpForm = () => {
+  /*
+    Sign Up Form component 
+    @param {object} - formFields
+    @param {string} - user email
+    @param {string} - user password
+    @param {string} - confirm password
+  */
+  const [formFields, setFormFields] = useState<FormFields>(defaultFormFields);
+  const { email, password, confirmPassword } = formFields;
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    /*
+      Handle Change function
+      @param {object} - e
+
+      destructures the name and value off the e.target object,
+      and then invokes the setFormFields function passing in 
+      previous formFields, and the new updated formField data. 
+    */
+    const { name, value } = e.target as HTMLInputElement;
+    setFormFields({ ...formFields, [name]: value });
+  };
+  const handleSubmit = async (e: FormEvent) => {
+    /*
+      Handle Submit functin
+      @param {object} - e
+      @param {string} - email
+      @param {string} - password 
+
+      Prevents the default refresh, 
+      and tries to invoke the createAuthUserWithEmailAndPassword 
+      passing in email, and password while catching the error if one
+      is thrown.
+    */
+    e.preventDefault();
+    try {
+      const user = await createAuthUserWithEmailAndPassword(email, password);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
     <>
       <div className={`${styles.sign_up_form} container`}>
         <h1>Sign Up</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className={styles.form_group}>
-            <input type="text" name="email" required />
-            <label htmlFor="">email</label>
+            <input
+              type="text"
+              name="email"
+              onChange={handleChange}
+              value={email}
+            />
+            <label htmlFor="email">email</label>
             <span className={styles.bar}></span>
           </div>
           <div className={styles.form_group}>
-            <input type="password" name="confirm_password" required />
-            <label htmlFor="">password</label>
-            <span className={styles.bar}></span>
-          </div>
-          <div className={styles.form_group}>
-            <input type="password" name="password" required />
+            <input
+              type="password"
+              name="password"
+              onChange={handleChange}
+              value={password}
+              required
+            />
             <label htmlFor="">confirm password</label>
+            <span className={styles.bar}></span>
+          </div>
+          <div className={styles.form_group}>
+            <input
+              type="password"
+              name="confirmPassword"
+              onChange={handleChange}
+              value={confirmPassword}
+              required
+            />
+            <label htmlFor="">password</label>
             <span className={styles.bar}></span>
           </div>
           <button className="btn">Sign Up</button>
