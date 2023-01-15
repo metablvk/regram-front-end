117dd1701075bfa8cc670b54e1ac496e22f008d7
metablvk <metablvk@gmail.com>
2023-01-15 12:52:36 -0700

    Added in redux

---
+++ store/user/user.selector.ts
@@ -0,0 +1,2 @@
+import { RootState } from 'store/store';
+export const selectCurrentUser = (state: RootState) => state.user.currentUser;
---
+++ utils/reducers/reducer.utils.ts
@@ -0,0 +1,4 @@
+export const createAction = (type: string, payload: object) => ({
+  type,
+  payload,
+});
