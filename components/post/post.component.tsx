f77cd8d4e5b8dff27b97a4eb8f0416b5444abd01
metablvk <metablvk@gmail.com>
2023-01-18 22:47:55 -0700

    Added like function into post

--- components/post/post.component.tsx
+++ components/post/post.component.tsx
@@ -1,44 +1,86 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './post.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
 import { IPost } from 'types/Post';
+import { addLike } from 'utils/firebase/firebase.utils';
+import { selectCurrentUser } from 'store/user/user.selector';
+import { useSelector } from 'react-redux';
+import { selectCurrentPost } from 'store/post/post.selector';
+import { setCurrentPost } from 'store/post/post.action';
 
 type Props = {
   post: IPost;
 };
 
 const Post: FC<Props> = ({ post }) => {
+  const currentUser = useSelector(selectCurrentUser);
+  const currentPost = useSelector(selectCurrentPost);
+
+  // WIP: Fix type error to use IPOST
+  const [postState, setPostState] = useState<any>(post);
+  const handleLike = async () => {
+    if (currentUser) {
+      try {
+        // Likes a post by passing in the id, and user id
+        if (post.id) {
+          const likedPost = await addLike(post.id, currentUser.uid);
+          // Get's the liked posts location
+
+          const likedPostLocation = currentPost.findIndex(
+            (obj: IPost) => obj.id === post.id
+          );
+          if (likedPost) {
+            const updatedPosts = currentPost;
+            updatedPosts[likedPostLocation] = likedPost;
+            setCurrentPost(updatedPosts);
+            setPostState(likedPost);
+            console.log(likedPost);
+          }
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
   return (
     <div className={styles.post}>
-      {post && (
+      {postState && (
         <>
-          {post.img && (
+          {postState.img && (
             <Image
-              src={post.img}
+              src={postState.img}
               height={200}
               width={200}
               alt='Stock image'
-              className={styles.post_img}
+              className={styles.postState_img}
             />
           )}
           <section className={styles.card}>
             <div className={styles.card_header}>
               <div>
-                <p>{post.likes} likes</p>
+                <p>{postState.likes.length} likes</p>
               </div>
               <div className={styles.controls}>
                 <FontAwesomeIcon icon={faComment} />
-                <FontAwesomeIcon icon={faHeart} />
+                <FontAwesomeIcon
+                  className={`${
+                    currentUser &&
+                    postState.likes.includes(currentUser.uid) &&
+                    styles.liked
+                  }`}
+                  icon={faHeart}
+                  onClick={handleLike}
+                />
               </div>
               <div>
-                <p className={styles.card_tag}>{post.username}</p>
+                <p className={styles.card_tag}>{postState.username}</p>
               </div>
             </div>
             <div className={styles.card_body}>
-              <p>{post.content}</p>
+              <p>{postState.content}</p>
             </div>
             <div className={styles.card_footer}>
               <Link href='/'>View All Comments</Link>
